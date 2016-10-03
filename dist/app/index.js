eb7055022a3bacea6b5add7d1ed361372ed4fdd6
Marc Despland <marc.despland@art122-5.net>
2016-10-03 10:37:48 +0200

    remove unwanted file from git

--- dist/app/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var environment_1 = require('./environment');
-exports.environment = environment_1.environment;
-var odyc3_component_1 = require('./odyc3.component');
-exports.Odyc3AppComponent = odyc3_component_1.Odyc3AppComponent;
-//# sourceMappingURL=index.js.map
